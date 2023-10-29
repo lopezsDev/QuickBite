786d2e96044559bbf3e9bb3ab97a6a3267f2fdf3
Samir López M <samirisraellopez21@gmail.com>
2023-10-29 12:21:55 -0600

    Se carga el proyecto

---
+++ QuickBite/src/main/java/com/example/QuickBite/QuickBiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickBiteApplication.class, args);
+	}
+
+}
---
+++ QuickBite/src/test/java/com/example/QuickBite/QuickBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
